56d7f5aa0e0490aa8a4172333f0cf9b21dddc18e
ahsanTradeling <mohamed.zaman@tradeling.com>
2022-01-31 17:19:52 +0400

    Depandencies added font awsome and styled components

---
+++ src/components/BaseCaoursel/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+`;
+
+const Slide = styled.div`
+
+`;
+
+const BaseCarousel = () => {
+        return (
+            <Wrapper>
+                <Slide>
+
+                </Slide>
+            </Wrapper>
+        );
+    }
+;
+
+export default BaseCarousel;
